d268dda73b5f13a4eeebce32a3e273e8bc3d3452
Jiaqi Chang <9193292222qq.com>
2024-07-11 15:47:20 +0800

    提交test5

---
+++ test5.m
@@ -0,0 +1,11 @@
+clc
+clear
+close all
+%% 提交流程
+%1、 新建的m文件
+%2、关闭所有文件      matlab里面需要增加一个! 
+%3、!git status      查看仓库的状态
+%3、!git add test5.m 添加到暂存区
+%4、!git status      查看仓库的状态
+%5、!git commit -m"  填写这次更改的信息" git commit只会提交暂存区的内容
+%6、!git log         查看仓库提交历史记录 可以使用 git log --oneline参数来查看简洁的提交记录
